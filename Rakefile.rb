bb9e68e67c97f39989e2648a7e08321a34b80470
Bryan Lee <leetucksing@gmail.com>
2013-07-15 22:21:50 +0800

    freshly out from ovan

---
+++ Rakefile.rb
@@ -0,0 +1,31 @@
+require 'rubygems'
+require 'optparse'
+require 'yaml'
+ 
+task :np do
+  OptionParser.new.parse!
+  ARGV.shift
+  title = ARGV.join(' ')
+ 
+  path = "_posts/#{Date.today}-#{title.downcase.gsub(/[^[:alnum:]]+/, '-')}.markdown"
+  
+  if File.exist?(path)
+  	puts "[WARN] File exists - skipping create"
+  else
+    File.open(path, "w") do |file|
+      file.puts YAML.dump(
+        {
+          'layout' => 'post',
+          'published' => false,
+          'title' => title,
+          'tags' => 'tech',
+          'post-link' =>'',
+        }
+          )
+      file.puts "---"
+    end
+  end
+  `open -a Byword.app #{path}`
+ 
+  exit 1
+end
